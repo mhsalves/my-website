0adaf61072c5f4243ca9c78c8a86ed50732bee42
Matheus Alves <matheus.alves@meliuz.com.br>
2019-04-08 01:25:06 -0400

    update(theme): Troca todas as funções por dicionários
    O motivo é que as funções aplicadas no tema estão influenciando nos testes dos styled components pois os mesmos estão sendo importados porém alguns não são usados.

--- bosons/themes/base/breakpoints.js
+++ bosons/themes/base/breakpoints.js
@@ -1,36 +1,12 @@
+/*
+ * Based in the rule mobile-first all breakpoints has only min-width.
+ * PS: the xs point is unnecessary because we don't need a "min-width: 0px"
+ */
 const breakpoints = {
-  xs: 0,
-  sm: 576,
-  md: 768,
-  lg: 992,
-  xl: 1200,
+  sm: '@media (min-width: 576px)',
+  md: '@media (min-width: 768px)',
+  lg: '@media (min-width: 992px)',
+  xl: '@media (min-width: 1200px)',
 };
 
-const keys = Object.keys(breakpoints);
-const unit = 'px';
-
-function up(key) {
-  return `@media (min-width: ${breakpoints[key]}${unit})`;
-}
-
-function between(start, end) {
-  const endIndex = keys.indexOf(end) + 1;
-
-  if (endIndex === keys.length) {
-    return up(start);
-  }
-
-  return `@media (min-width: ${breakpoints[start]}${unit}) and (max-width: ${breakpoints[keys[endIndex]]}${unit})`;
-}
-
-function get(key) {
-  if (!breakpoints[key]) return null;
-
-  return `${breakpoints[key]}${unit}`;
-}
-
-export default {
-  up,
-  between,
-  get,
-};
+export default breakpoints;
--- bosons/themes/base/colors.js
+++ bosons/themes/base/colors.js
@@ -2,8 +2,6 @@ const main = {
   primary: '#BD5D38',
 };
 
-const mainKeys = Object.keys(main);
-
 const defaults = {
   coal: {
     default: '#868E96',
@@ -20,31 +18,4 @@ const colors = {
   ...main, ...defaults,
 };
 
-/*
- * This function need receive a label and return a hex color of Style Guide
- *
- * If didn't find a color may return the primary.
- *
- * The label can be a composed or simple string
- * For example:
- * - coal dark
- * - primary
- * - silver
- *
- * In the case of combined colors need return the "default"
- * For example:
- * - silver -> silver deafult
- * - coal -> coal deafult
- */
-
-const getColor = (label) => {
-  if (label.length === 0) return main.primary;
-
-  const [name, variant] = label.split(' ');
-
-  if (mainKeys.includes(name)) return colors[name];
-
-  return colors[name][variant || 'default'] || colors.primary;
-};
-
-export default getColor;
+export default colors;
